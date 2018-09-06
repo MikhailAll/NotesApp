45bd274d86a4d17b77a2ee0fbde1cf3a29889053
Misha Allakhverdow <Xangton@gmail.com>
2018-09-06 11:49:15 +0300

    Add project files.

---
+++ NoteBook/Models/Notes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteBook.Models
+{
+    public class Notes
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Text { get; set; }
+    }
+}
