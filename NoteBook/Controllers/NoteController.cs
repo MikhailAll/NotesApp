158635d7b346f55544cf2f69a8a44448f211e784
Misha Allakhverdow <Xangton@gmail.com>
2018-09-13 21:07:23 +0300

    Complete project redesign

---
+++ NoteBook/Controllers/NoteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoteBook.Controllers
+{
+    public class NoteController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddNote()
+        {
+            return View();
+        }
+    }
+}
